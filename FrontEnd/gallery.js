9d6229d82bd3c7f8523612af5827d5e8960f42c0
TL <lambert.tom@hotmail.fr>
2023-06-12 13:10:15 +0200

    fix: Refactorisation des fonctions et suppresion du localStorage

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -1,61 +1,91 @@
 /////////////////////////////////////////////////
+/**
+ * Fonction d'Affichage des travaux sur le site
+ */
+/////////////////////////////////////////////////
 
-// Variable pour récupérer le localStorage (voir ligne 19)
-let worksData = JSON.parse(localStorage.getItem("worksData")) || [];
+// Le paramètre works concerne la variable qui fetch les données de l'API (l.41)
+function displayWorks(works) {
+  const gallery = document.querySelector(".gallery");
+
+  gallery.innerHTML = ""; // On efface le contenu actuel de la galerie
+
+  for (let i = 0; i < works.length; i++) {
+    const work = works[i]; // Cette variable stocke l'index des travaux convertis en JSON plus haut
+
+    const galleryFigure = document.createElement("figure"); // Création de la balise dédiée aux figures
+
+    const galleryImage = document.createElement("img"); // Création de la balise dédiée aux images
+    galleryImage.src = work.imageUrl; // On va récupérer la source de l'image dans l'API
+    galleryImage.alt = work.title; // On va récupérer l'alt de l'image dans l'API
+
+    const galleryFigcaption = document.createElement("figcaption"); // Création de la balise dédiée aux figcaptions
+    galleryFigcaption.innerText = work.title; // On va récupérer le titre de l'image dans l'API
+
+    // On rattache les balises créées à leurs sections respectives
+    galleryFigure.appendChild(galleryImage);
+    galleryFigure.appendChild(galleryFigcaption);
+    gallery.appendChild(galleryFigure);
+  }
+}
 
 /////////////////////////////////////////////////
 /**
  * Fonction de Récupération des travaux depuis l'API
  */
 /////////////////////////////////////////////////
+
 async function fetchWorks() {
   try {
     const response = await fetch("http://localhost:5678/api/works"); // Attente de la réponse
     const works = await response.json(); // Attente de la conversion en JSON
 
-    // Transformation de la variable works en string JSON
-    const worksStringify = JSON.stringify(works);
-    // On Stock la chaîne de caractères dans le localStorage (clé, valeur)
-    localStorage.setItem("worksData", worksStringify);
-
-    // Gestion d'Affichage des travaux récupérés,on retourne la fonction qui affiche les travaux délcaré plus bas
-    displayWorks(worksData);
+    displayWorks(works); // Appel à la fonction (l.8) pour afficher les travaux
 
     /////////////////////////////////////////////////
     /**
-     * Fonction de filtrage des boutons
+     *           Filtrage des boutons
      */
     /////////////////////////////////////////////////
 
-    // ***** Bouton Tous ***** //
+    // Rattachement et Ecoute du bouton "Tous"
     const btnTous = document.querySelector(".btn-tous");
-    btnTous.addEventListener("click", function () {
-      // On efface le contenu actuel de la galerie (on double la query qui est aussi plus bas)
-      const gallery = document.querySelector(".gallery");
-      gallery.innerHTML = "";
-      // On retourne à nouveau la fonction qui affiche les travaux
-      displayWorks(worksData);
+    btnTous.addEventListener("click", () => {
+      displayWorks(works); // Fait a nouveau appel à la fonction pour afficher les travaux
     });
 
-    // ***** Bouton Objets ***** //
-    const btnObjets = document.querySelector(".btn-objets");
-    btnObjets.addEventListener("click", function () {
-      const filteredWorks = worksData.filter((work) => work.categoryId === 1);
-      displayWorks(filteredWorks);
-    });
+    // On Fetch la section "category" de l'API cette fois
+    const category = await fetch("http://localhost:5678/api/categories"); // Attente de la réponse
+    const btnFiltre = await category.json(); // Attente de la conversion en JSON
 
-    // ***** Bouton Appartements ***** //
-    const btnAppartements = document.querySelector(".btn-appartements");
-    btnAppartements.addEventListener("click", function () {
-      const filteredWorks = worksData.filter((work) => work.categoryId === 2);
-      displayWorks(filteredWorks);
-    });
+    let baliseFiltre = document.querySelector(".filtres"); // On va chercher la balise HTML
 
-    // ***** Bouton Hotels & Restaurants ***** //
-    const btnHotEtRes = document.querySelector(".btn-hot-et-res");
-    btnHotEtRes.addEventListener("click", function () {
-      const filteredWorks = worksData.filter((work) => work.categoryId === 3);
-      displayWorks(filteredWorks);
+    // La boucle qui va parcourir l'API et créer un nouveau bouton pour chaque catégorie
+    btnFiltre.forEach((element) => {
+      let newButton = document.createElement("button");
+
+      // On donne une class a la balise bouton
+      newButton.setAttribute(
+        "class",
+        element.name
+          .replace(/\s+/g, "-") // Remplace les espaces par des tirets
+          .replace(/&/g, "-") // Supprime les "&" qui sont mal supportés en CSS
+          .replace(/-+/g, "-") // Remplace les enchaînements de tirets par un seul
+          .toLowerCase() // Tout en minuscules
+      );
+
+      newButton.textContent = element.name; // Et on détermine le texte à l'intérieur
+
+      baliseFiltre.appendChild(newButton); // la variable qui contient la balise va hériter des boutons
+
+      // Écoute du clic sur les boutons filtre
+      newButton.addEventListener("click", () => {
+        const filteredWorks = works.filter(
+          (work) => work.categoryId === element.id
+        );
+
+        displayWorks(filteredWorks); // Appel de la fonction (l.8) avec comme paramètre la variable filtrante
+      });
     });
 
     // Gestion des erreurs
@@ -66,41 +96,6 @@ async function fetchWorks() {
     );
   }
 }
-// On appelle ensuite la fonction
+
+// Appel de la fonction de Récupération des travaux depuis l'API (l.38)
 fetchWorks();
-
-/////////////////////////////////////////////////
-/**
- * Fonction d'Affichage des travaux sur le site
- */
-/////////////////////////////////////////////////
-function displayWorks(works) {
-  const gallery = document.querySelector(".gallery");
-
-  // On efface le contenu actuel de la galerie
-  gallery.innerHTML = "";
-
-  for (let i = 0; i < works.length; i++) {
-    // Cette variable stocke l'index des travaux convertis en JSON plus haut
-    const work = works[i];
-
-    // Creation de la balise dédiée aux figures
-    const galleryFigure = document.createElement("figure");
-
-    // Création de la balise dédiée aux images
-    const galleryImage = document.createElement("img");
-    // On va récupérer la source de l'image dans l'API
-    galleryImage.src = work.imageUrl;
-    // On va récupérer l'alt de l'image dans l'API
-    galleryImage.alt = work.title;
-
-    // Création de la balise dédiée aux figcaptions
-    const galleryFigcaption = document.createElement("figcaption");
-    galleryFigcaption.innerText = work.title;
-
-    // On rattache les balises créées à leurs sections respectives
-    galleryFigure.appendChild(galleryImage);
-    galleryFigure.appendChild(galleryFigcaption);
-    gallery.appendChild(galleryFigure);
-  }
-}

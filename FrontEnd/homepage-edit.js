992a97d84e06608472f42763771fa27dd6cf24f3
TL <lambert.tom@hotmail.fr>
2023-06-14 17:17:56 +0200

    style: Ajout de la homepage en version editable

--- FrontEnd/homepage-edit.js
+++ FrontEnd/homepage-edit.js
@@ -6,18 +6,24 @@
 
 const token = localStorage.getItem("token"); // On récupère le token du localStorage
 
+const editDisplay = document.querySelector(".edit-section"); // On va chercher la section edit login dans le DOM
+
 const loginDisplay = document.querySelector(".login"); // On va chercher l'élément login dans le DOM
 const logoutDisplay = document.querySelector(".logout"); // On va chercher l'élément logout dans le DOM
 
+const modifPhotoDisplay = document.querySelector(".modif-photo"); // On va chercher l'élément modif photo dans le DOM
 const modifProjetsDisplay = document.querySelector(".modif-projets"); // On va chercher l'élément modif projet dans le DOM
 
 const filterDisplay = document.querySelector(".filtres"); // On va chercher l'élément filtres dans le DOM
 
 // Le token existe dans le localStorage
 if (token !== null) {
+  editDisplay.style.display = "flex"; // on active la section edit
+
   loginDisplay.style.display = "none"; // on desactive l'élément login
   logoutDisplay.style.display = "block"; // on active l'élément logout
 
+  modifPhotoDisplay.style.display = "flex"; // on active la modif projets
   modifProjetsDisplay.style.display = "flex"; // on active la modif projets
 
   filterDisplay.style.display = "none"; // on desactive les filtres
